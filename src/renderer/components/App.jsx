834aaecedee2e6f473e11a34e0d7511b4bc7c983
Krzysztof Kakolewski <krzysztofkakolewski@googlemail.com>
2025-01-08 13:53:36 +0000

    blank main.js and test file for App.jsx

---
+++ src/renderer/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to your Computer time limiter program</h1>
+            <p>Adjust settings and monitor usage directly from this program.</p>
+        </div>
+    );
+}
+
+export default App;
