98e505801ecab816395ef9b4a320a829830096ac
Krzysztof Kakolewski <krzysztofkakolewski@googlemail.com>
2025-01-08 14:58:49 +0000

    Fix index.js imports and update @babel/plugin-proposal-private-property-in-object for dev build issue

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import './scss/main.scss';
+
 
 const rootElement = document.createElement('div');
 rootElement.id = 'root';
