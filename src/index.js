d8e5dc9cff6711ea71ae32eec5d2fd29f9012c76
Krzysztof Kakolewski <krzysztofkakolewski@googlemail.com>
2025-01-08 21:23:22 +0000

    index.js manual creataion of root element removed

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
 
-const rootElement = document.createElement('div');
-rootElement.id = 'root';
-document.body.appendChild(rootElement);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
