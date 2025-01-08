d42767a72ce3561563a07a6e0ad1df53d4a580e9
Krzysztof Kakolewski <krzysztofkakolewski@googlemail.com>
2025-01-08 14:32:58 +0000

    index.js update

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './scss/main.scss';
 
+const rootElement = document.createElement('div');
+rootElement.id = 'root';
+document.body.appendChild(rootElement);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
