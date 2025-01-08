e55b21c9061ce3353b56ebc9be4529b34de0be56
Krzysztof Kakolewski <krzysztofkakolewski@googlemail.com>
2025-01-08 14:26:56 +0000

    test version of main.js

---
+++ src/main/main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+
+
+    mainWindow.loadFile(path.join(__dirname, '../build/index.html'));
+}
+
+app.whenReady().then(createWindow);
